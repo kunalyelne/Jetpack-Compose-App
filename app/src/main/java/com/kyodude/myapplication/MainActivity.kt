03ad10c14c1fc35e25943a5f94bc82e8d6fa1ef1
Kunal Yelne <kunal17058@gmail.com>
2021-07-31 15:43:01 +0530

    Initial commit

---
+++ app/src/main/java/com/kyodude/myapplication/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.kyodude.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.kyodude.myapplication.ui.theme.ComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Kunal")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    ComposeAppTheme {
+        Greeting("Kunal")
+    }
+}
