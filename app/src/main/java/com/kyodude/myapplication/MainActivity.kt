8bd6eabb5d097530ddf18f5ca1f0c02f9d969ca3
Kunal Yelne <kunal17058@gmail.com>
2021-08-05 22:54:34 +0530

    Add LazyColumn Lists

--- app/src/main/java/com/kyodude/myapplication/MainActivity.kt
+++ app/src/main/java/com/kyodude/myapplication/MainActivity.kt
@@ -3,14 +3,23 @@ package com.kyodude.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kyodude.myapplication.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +35,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+data class Message(val author: String, val body: String)
 @Composable
 fun Greeting(name: String) {
     Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
@@ -36,10 +45,69 @@ fun Greeting(name: String) {
 
 }
 
-@Preview(showSystemUi = true)
 @Composable
-fun DefaultPreview() {
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.droid),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        Column(modifier = Modifier.clickable { isExpanded=!isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+}
+
+//@Preview(showSystemUi = true)
+//@Composable
+//fun DefaultPreview() {
+//    ComposeAppTheme {
+//        Greeting("Kunal")
+//    }
+//}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewConversation() {
     ComposeAppTheme {
-        Greeting("Kunal")
+        Conversation(SampleData.conversationSample)
     }
 }
